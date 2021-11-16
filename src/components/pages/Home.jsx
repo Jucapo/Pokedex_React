bd8ad05bdcb0dc4ad4473399adf76c7c54a84b71
Juan Posso <juan.posso@yuxiglobal.com>
2021-11-16 01:37:47 -0500

    Add Popup detail

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,16 +1,34 @@
-import React, { useState, useContext, useMemo } from "react";
+import React, { useState, useContext, useMemo, Fragment } from "react";
 import AppContext from "../../context/AppContext";
 import axios from "axios";
+
 import "../../styles/_styles.css";
 
 import homeIcon from "../../assets/home.png";
 import catchIcon from "../../assets/catch.png";
 import detailIcon from "../../assets/details.png";
+import closeIcon from "../../assets/close-icon.png";
+
+import { Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.css";
 
 const Home = () => {
-  const [pokemonData, setPokemonData] = useState([]);
-  const { state, setState } = useContext(AppContext);
+  const { state } = useContext(AppContext);
   const pokemons = state.allPokemon;
+  const [pokemonData, setPokemonData] = useState([]);
+  const [modal, setModal] = useState(false);
+
+  const openModal = () => {
+    setModal(!modal);
+  };
+
+  const modalStyle = {
+    backgroundColor: "#f5f5f5",
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+  };
 
   const useSearchPokemon = (pokemons) => {
     const [query, setQuery] = useState("");
@@ -51,58 +69,76 @@ const Home = () => {
   const { query, setQuery, filteredPokemon } = useSearchPokemon(pokemons);
 
   return (
-    <div className="homeContainer">
-      <div className="searchContainer">
-        <div className="formContainer">
-          <form onSubmit={handleSubmit}>
-            <label>
-              <input
-                type="text"
-                value={query}
-                onChange={(e) => {
-                  setQuery(e.target.value);
-                }}
-                placeholder="Enter Pokemon Name"
-              />
-            </label>
-          </form>
+    <Fragment>
+      <div className="homeContainer">
+        <div className="searchContainer">
+          <div className="formContainer">
+            <form onSubmit={handleSubmit}>
+              <label>
+                <input
+                  type="text"
+                  value={query}
+                  onChange={(e) => {
+                    setQuery(e.target.value);
+                  }}
+                  placeholder="Enter Pokemon Name"
+                />
+              </label>
+            </form>
+          </div>
+          <img className="homeIcon" src={homeIcon} alt="home" />
         </div>
-        <img className="homeIcon" src={homeIcon} alt="home" />
-      </div>
-      <div className="detailContainer">
-        <div className="listContainer">
-          {filteredPokemon.map((pokemon) => {
-            return (
-              <div key={pokemon.name} className="listItem" onClick={getPokemon.bind(this, pokemon.name)}>
-                {capitalize(pokemon.name)}
-              </div>
-            );
-          })}
-        </div>
-        <div className="cardContainer">
-          {pokemonData.map((data) => {
-            return (
-              <div className="cardBodyContainer">
-                <div className="leftColumnCard">
-                  <div className="pokemonName">{capitalize(data.name)}</div>
-                  <img className="pokemonImg" src={data.sprites["front_default"]} alt="pokemon" />
+        <div className="detailContainer">
+          <div className="listContainer">
+            {filteredPokemon.map((pokemon) => {
+              return (
+                <div key={pokemon.name} className="listItem" onClick={getPokemon.bind(this, pokemon.name)}>
+                  {capitalize(pokemon.name)}
                 </div>
-                <div className="rigthColumnCard">
-                  <div className="buttonContianer">
-                    <img className="catchIcon" src={catchIcon} alt="catch" />
-                    <h4>CATCH</h4>
+              );
+            })}
+          </div>
+          <div className="cardContainer">
+            {pokemonData.map((data) => {
+              return (
+                <div className="cardBodyContainer">
+                  <div className="leftColumnCard">
+                    <div className="pokemonName">{capitalize(data.name)}</div>
+                    <img className="pokemonImg" src={data.sprites["front_default"]} alt="pokemon" />
                   </div>
-                  <div className="buttonContianer ml1">
-                    <img className="detailsIcon" src={detailIcon} alt="datails" />
-                    <h4>DETAIL</h4>
+                  <div className="rigthColumnCard">
+                    <div className="buttonContianer">
+                      <img className="catchIcon" src={catchIcon} alt="catch" />
+                      <h4>CATCH</h4>
+                    </div>
+                    <div className="buttonContianer ml1" onClick={() => openModal()}>
+                      <img className="detailsIcon" src={detailIcon} alt="datails" />
+                      <h4>DETAIL</h4>
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
       </div>
-    </div>
+      {pokemonData.map((data) => {
+        return (
+          <Modal isOpen={modal} style={modalStyle}>
+            <ModalBody>
+              <div className="headerPopUp">
+                <div className="pokemonNamePopUp">{capitalize(data.name)}</div>
+                <img className="closeIcon" src={closeIcon} alt="close" />
+              </div>
+              <div className="bodyPopUp">
+                <img className="pokemonImg" src={data.sprites["front_default"]} alt="pokemon" />
+                <div>ATAKCS</div>
+              </div>
+            </ModalBody>
+          </Modal>
+        );
+      })}
+    </Fragment>
   );
 };
 
